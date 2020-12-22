737e3f52de2f0982e230c4da7b4a035144ab1fb3
Alexander Bai <vhurryharry@gmail.com>
2020-12-22 10:36:10 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## Absorber Protocol Dashboard
---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "assets/styles/tailwind.css";
+
+// layouts
+
+import Admin from "layouts/Admin.js";
+import Auth from "layouts/Auth.js";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+      <Route path="/auth" component={Auth} />
+      <Route path="/" exact component={Admin} />
+
+      <Redirect from="*" to="/" />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
