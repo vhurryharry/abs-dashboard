4fb14151fe9f09a89138ed6901c0e0410fc6b50a
Alexander Bai <vhurryharry@gmail.com>
2020-12-25 10:16:08 -0500

    Changed price chart color

--- src/components/Cards/CardLineChart.js
+++ src/components/Cards/CardLineChart.js
@@ -17,8 +17,8 @@ const CardLineChart = ({ data }) => {
             {
               label: "USD",
               fill: false,
-              backgroundColor: "#fff",
-              borderColor: "#fff",
+              backgroundColor: "#4c51bf",
+              borderColor: "#4c51bf",
               data: data.usd.prices.map((price) => price[1]),
             },
           ],
@@ -29,11 +29,11 @@ const CardLineChart = ({ data }) => {
           title: {
             display: false,
             text: "Price History",
-            fontColor: "white",
+            fontColor: "rgba(0,0,0,.4)",
           },
           legend: {
             labels: {
-              fontColor: "white",
+              fontColor: "rgba(0,0,0,.4)",
             },
             align: "end",
             position: "bottom",
@@ -50,18 +50,18 @@ const CardLineChart = ({ data }) => {
             xAxes: [
               {
                 ticks: {
-                  fontColor: "rgba(255,255,255,.7)",
+                  fontColor: "rgba(0,0,0,.6)",
                 },
                 display: true,
                 scaleLabel: {
                   display: false,
-                  fontColor: "white",
+                  fontColor: "rgba(0,0,0,.4)",
                 },
                 gridLines: {
                   display: false,
                   borderDash: [2],
                   borderDashOffset: [2],
-                  color: "rgba(33, 37, 41, 0.3)",
+                  color: "red",
                   zeroLineColor: "rgba(0, 0, 0, 0)",
                   zeroLineBorderDash: [2],
                   zeroLineBorderDashOffset: [2],
@@ -71,18 +71,18 @@ const CardLineChart = ({ data }) => {
             yAxes: [
               {
                 ticks: {
-                  fontColor: "rgba(255,255,255,.7)",
+                  fontColor: "rgba(0,0,0,.6)",
                 },
                 display: true,
                 scaleLabel: {
                   display: false,
-                  fontColor: "white",
+                  fontColor: "rgba(0,0,0,.4)",
                 },
                 gridLines: {
                   borderDash: [3],
                   borderDashOffset: [3],
                   drawBorder: false,
-                  color: "rgba(255, 255, 255, 0.15)",
+                  color: "rgba(0,0,0,.4)",
                   zeroLineColor: "rgba(33, 37, 41, 0)",
                   zeroLineBorderDash: [2],
                   zeroLineBorderDashOffset: [2],
@@ -100,14 +100,16 @@ const CardLineChart = ({ data }) => {
 
   return (
     <>
-      <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded bg-gray-800">
+      <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded bg-white">
         <div className="rounded-t mb-0 px-4 py-3 bg-transparent">
           <div className="flex flex-wrap items-center">
             <div className="relative w-full max-w-full flex-grow flex-1">
-              <h6 className="uppercase text-gray-200 mb-1 text-xs font-semibold">
+              <h6 className="uppercase text-gray-500 mb-1 text-xs font-semibold">
                 Price
               </h6>
-              <h2 className="text-white text-xl font-semibold">Last 7 days</h2>
+              <h2 className="text-gray-800 text-xl font-semibold">
+                Last 7 days
+              </h2>
             </div>
           </div>
         </div>
